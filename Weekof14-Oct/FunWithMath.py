bd6553679093188f9d8dc37153073810693facaf
100111079 <kylervillarreal@icloud.com>
2019-10-15 08:34:19 -0500

    accumulators and tasks

--- Weekof14-Oct/FunWithMath.py
+++ Weekof14-Oct/FunWithMath.py
@@ -22,6 +22,23 @@ for sides in range(8, 100, 8):
 
 # experiment with the loop above alongside the actual value of pi. How many sides does it take to make the two close?
 
+# Accumulators
+
+acc = 0
+for x in range(1, 6):
+    acc = acc + x
+
+print(acc)
+
+# compute the sum of the first 100 even numbers
+# compute the sum of the first 50 odd numbers
+# compute the average of the first 100 odd numbers
+# write a function that returns the average of the first N numbers, where N is a parameter
+# write a function called factorial that computes the product of the first N numbers, where N is a parameter
+# Each number in the Fibonacci sequence is the sum of the previous two numbers.
+#  The first two numbers in the sequence are 1 and 1. Compute the 10th Fibonacci number
+# write a function to compute the Nth Fibonacci number, where N is a parameter
+#  You may assume that N will be greater than or equal to 3.
 
 
 
